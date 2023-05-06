1653d4860dda75dc0f415f67cc15deb090313432
Stanislav <jp.arbitrage@gmail.com>
2023-05-06 12:03:44 +0200

    get_all_orders

--- src/futures/account.rs
+++ src/futures/account.rs
@@ -481,12 +481,13 @@ impl FuturesAccount {
             .get_signed(API::Futures(Futures::OpenOrders), Some(request))
     }
 
-    pub fn get_all_orders<S>(&self, symbol: S) -> Result<Vec<crate::futures::model::Order>>
+    pub fn get_all_orders<S>(&self, symbol: S, limit: usize) -> Result<Vec<crate::futures::model::Order>>
     where
         S: Into<String>,
     {
         let mut parameters: BTreeMap<String, String> = BTreeMap::new();
         parameters.insert("symbol".into(), symbol.into());
+        parameters.insert("limit".into(), limit.into());
         let request = build_signed_request(parameters, self.recv_window)?;
         self.client
             .get_signed(API::Futures(Futures::AllOrders), Some(request))
