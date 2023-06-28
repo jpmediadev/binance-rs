d25af5e1c1392e053fe8de226aafc0fc11750721
Stanislav <jp.arbitrage@gmail.com>
2023-06-28 11:44:56 +0200

    add CancelReplace for Spot

--- src/futures/account.rs
+++ src/futures/account.rs
@@ -333,11 +333,11 @@ impl FuturesAccount {
         let mut parameters = BTreeMap::new();
         parameters.insert("symbol".into(), symbol.into());
         parameters.insert("side".into(), side.into());
-        parameters.insert("price".into(), price.into());
+        parameters.insert("price".into(), price.to_string());
         parameters.insert("origClientOrderId".into(), client_order_id.into());
 
         if !is_close_position{
-            parameters.insert("quantity".into(), quantity.into());
+            parameters.insert("quantity".into(), quantity.to_string());
         }
 
         let request = build_signed_request(parameters, self.recv_window)?;
